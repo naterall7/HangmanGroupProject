2157dced6c067f97ace38c10b54d90a7cd086b5b
naterall7 <nrall001@student.franciscan.edu>
2024-04-18 12:13:34 -0400

    added file for list of requirements and added keypress function

--- src/HangmanGroupProject.py
+++ src/HangmanGroupProject.py
@@ -26,3 +26,26 @@ def intro():
 intro()
 
 # keypress functions
+window = tk.Tk()
+window.title("Hangman")
+
+frame = tk.Frame(master=window, relief="sunken", width=100, height=100)
+frame.pack()
+label = tk.Label(master=frame, text="")
+label.pack()
+
+def submit_guess():
+    print(label["text"])
+
+button = tk.Button(master=frame, relief="raised", text="Submit", bg="red", fg="white", width=5, height=5, command=submit_guess)
+button.pack()
+
+def update(label, text):
+    label.config(text=text)
+
+def handle_keypress(event):
+    key = event.char
+    update(label, key)
+
+window.bind("<Key>", handle_keypress)
+window.mainloop()
---
+++ src/ListOfRequirements.py
@@ -0,0 +1,12 @@
+# Week #1 - Printing - Completed
+# Week #2 - Input - Completed
+# Week #3 - Functions - Completed
+# Week #4 - If Statements - Completed
+# Week #5 - 
+# Week #6 - 
+# Week #7 - 
+# Week #8 - 
+# Week #9 - 
+# Week #10 - 
+# Week #11 - 
+# Week #12 - 
