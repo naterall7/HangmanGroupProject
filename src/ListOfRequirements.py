94ac03017b3f147c246d3c0eeeb4e8a5cbfaebfb
naterall7 <nrall001@student.franciscan.edu>
2024-05-04 17:25:13 -0400

    All of Requirements Completed

--- src/ListOfRequirements.py
+++ src/ListOfRequirements.py
@@ -6,7 +6,7 @@
 # Week #6 - Accumulator Pattern - Completed
 # Week #7 - Tuple, Set, Dictionary - Completed
 # Week #8 - Exam (Ignore)
-# Week #9 - Classes, Objects, Inheritance, Scope
+# Week #9 - Classes, Objects, Inheritance, Scope - Completed
 # Week #10 - Tkinter Widgets - Completed
 # Week #11 - Tkinter Geometry Managers and Events and Interactions - Completed
 # Week #12 - Exam (Ignore)
